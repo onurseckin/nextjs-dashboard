aaa88ea76b91d23a2044776f0cffdba0de971457
Onur Seckin Senoglu <onurseckinsenoglu@gmail.com>
2025-03-04 01:56:26 -0800

    static, dynamic and streaming data fetching, suspense

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,27 +103,15 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      // seedUsers(),
-      seedCustomers(),
-      // seedInvoices(),
-      // seedRevenue(),
-    ]);
+    // const result = await sql.begin((sql) => [
+    // seedUsers(),
+    // seedCustomers(),
+    // seedInvoices(),
+    // seedRevenue(),
+    // ]);
 
     return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
 }
-
-// export async function GET() {
-//   return Response.json({
-//     message:
-//       "Uncomment this file and remove this line. You can delete this file when you are finished.",
-//   });
-//   // try {
-//   // 	return Response.json(await listInvoices());
-//   // } catch (error) {
-//   // 	return Response.json({ error }, { status: 500 });
-//   // }
-// }
